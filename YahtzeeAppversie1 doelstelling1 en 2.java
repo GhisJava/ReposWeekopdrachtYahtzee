67ac28370a645942646bc2a2efc367833abeee85
GhisJava <g.fardaoussi@workingspirit.nl>
2019-04-24 15:51:31 +0200

    Doelstelling 1 en 2

---
+++ YahtzeeAppversie1 doelstelling1 en 2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class YahtzeeApp {
+
+	public static void main(String[] args) {
+		System.out.println("Druk op 'Enter' om door te gaan of druk op 'q' om het spel te beëindigen:");
+		YahtzeeSpel spel = new YahtzeeSpel();
+		spel.spelen();		
+	}
+}
+
+class YahtzeeSpel {
+	
+	void spelen() {
+		Scanner sc = new Scanner(System.in);
+		char quit = 'q';
+		String enter = "";			
+
+		boolean doorgaan = true; 									 
+		while (doorgaan) {
+			String Toets = sc.nextLine();
+		  if (Toets.equals("q")) {
+				System.out.println("Het spel is gestopt");
+				doorgaan = false;
+				
+			} else if (Toets.equals(enter))
+				System.out.println("");
+		  
+				else if ((Toets != enter) || (Toets != "q")) {
+				System.out.println("Toets een 'enter' om door te gaan of een 'q' om te stoppen:");
+			}
+		}
+	}  
+}
